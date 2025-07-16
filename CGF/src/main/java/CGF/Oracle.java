7fa2cd8cb7523b3d0619651c0bf6d88a3a596a73
Fatih ÖZTÜRK <58885523+fatihozturkyytu@users.noreply.github.com>
2025-07-16 11:24:03 +0300

    Create Oracle.java

---
+++ CGF/src/main/java/CGF/Oracle.java
@@ -0,0 +1,12 @@
+package CGF;
+
+public class Oracle {
+    
+}
+
+
+/*
+ * Oracle'da Oluşturma, okuma, güncelleme ve silme gibi işlemlerin
+ * yapılacağı sınıf.
+ * arayan id, aranan id, kullanim tipi, süresi, tarihi
+ */
