e96bfb214b1e9c2226dc1a0cdaf3222b50456c05
ahmetonuk <121580255+ahmetonuk1@users.noreply.github.com>
2025-07-21 01:18:11 +0300

    Delete hazelcast-client-demo directory

--- hazelcast-client-demo/src/main/java/org/i2i/kotam/utils/constants/StringConstants.java
+++
@@ -1,6 +0,0 @@
-package org.i2i.kotam.utils.constants;
-
-public class StringConstants {
-    public static final String HAZELCAST_IP = "34.140.179.239"; // örnek IP
-    public static final String MAP_NAME = "usage-data-map";
-}
