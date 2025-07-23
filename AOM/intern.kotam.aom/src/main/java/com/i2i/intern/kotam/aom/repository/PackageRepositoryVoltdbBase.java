eef3824ce08c00153d33ad7265a735a979f1e8e8
zelihapp <120737807+zelihapp@users.noreply.github.com>
2025-07-23 17:34:07 +0300

    Add AOM Module

---
+++ AOM/intern.kotam.aom/src/main/java/com/i2i/intern/kotam/aom/repository/PackageRepositoryVoltdbBase.java
@@ -0,0 +1,28 @@
+/*
+
+package com.i2i.intern.kotam.aom.repository;
+
+import com.i2i.intern.kotam.aom.model.PackageEntity;
+
+import java.util.Optional;
+
+public interface PackageRepositoryVoltdbBase {
+
+
+    //MSISDN numarasına göre paketin adını döner.
+    Optional<String> getPackageNameByMsisdn(String msisdn);
+
+    //Paket ID'sine göre paketin adını döner.
+    Optional<String> getPackageNameByPackageId(Long packageId);
+
+
+     //Paket ID'sine göre tüm paket bilgilerini döner.
+     Optional<PackageEntity> getPackageInfoByPackageId(Long packageId);
+
+
+     //MSISDN'e göre müşteriye tanımlı paketin tüm bilgilerini döner.
+    Optional<PackageEntity> getPackageInfoByMsisdn(String msisdn);
+}
+
+ */
+
