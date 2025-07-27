c1e7f51d8f0aff4bcab8145bbce5e12c539c244a
zelihapp <120737807+zelihapp@users.noreply.github.com>
2025-07-27 03:34:13 +0300

    AOM modülünde güncellemeler yapıldı

---
+++ AOM/intern.kotam.aom/src/main/java/com/i2i/intern/kotam/aom/dto/request/NotificationRequestDTO.java
@@ -0,0 +1,25 @@
+/*
+package com.i2i.intern.kotam.aom.dto.request;
+
+public class NotificationRequestDTO {
+    private String msisdn;
+    private String message;
+
+    // Getters and Setters
+    public String getMsisdn() {
+        return msisdn;
+    }
+
+    public void setMsisdn(String msisdn) {
+        this.msisdn = msisdn;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+*/
